2bf5da64f72cabc2e542f52ef92fc17cb448b1d3
k3lly003 <ntirukelly@gmail.com>
2024-05-05 18:42:40 +0200

    initial commit

--- dozit_board/src/Pages/Boards/index.tsx
+++ dozit_board/src/Pages/Boards/index.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from  "react-beautiful-dnd";
+import { Columns } from "../../types";
+
 
 const Boards = () => {
+  const [columns, setColumns] = useState<Columns>(Board)
   return (
-    <div>Boards</div>
+    <>
+      <DragDropContext onDragEnd={(result: any)=> console.log(result)}>
+        <div className="w-full flex items-start justify-between px-5 pb-8 md:gap-0 gap-10"></div>
+      </DragDropContext>
+    </>
   )
 }
 
